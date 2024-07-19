65a7574d9a8a1c8fc3c68393aa498479e0502cef
malekshehadeh <malek.shehadeh96@gmail.com>
2024-07-20 01:47:02 +0300

    First

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 import Logout from "./pages/Logout";
-
-import NavBar from "./components/NavBar";
 
 import Home from "./pages/Home";
 import LogIn from "./pages/LogIn";
@@ -19,29 +15,29 @@ import Details from "./pages/Details";
 
 import Footer from "./components/Footer";
 import { Link } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-   <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="SignUp" element={<SignUp />} />
-        <Route path="LogIn" element={<LogIn />} />
-        <Route path="Logout" element={<Logout />} />
-        <Route path="ContactUs" element={<ContactUs />} />
-        <Route path="OurStory" element={<OurStory />} />
-        <Route path="Profile" element={<Profile />} />
-        <Route path="Catalog" element={<Catalog />} />
-        <Route path="Admin" element={<Admin />} />
-        <Route path="Booking" element={<Booking />} />
-        <Route path="Details" element={<Details />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="SignUp" element={<SignUp />} />
+          <Route path="LogIn" element={<LogIn />} />
+          <Route path="Logout" element={<Logout />} />
+          <Route path="ContactUs" element={<ContactUs />} />
+          <Route path="OurStory" element={<OurStory />} />
+          <Route path="Profile" element={<Profile />} />
+          <Route path="Catalog" element={<Catalog />} />
+          <Route path="Admin" element={<Admin />} />
+          <Route path="Booking" element={<Booking />} />
+          <Route path="Details" element={<Details />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
-
   );
 }
 
